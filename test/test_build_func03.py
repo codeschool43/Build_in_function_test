a12efc03f6b05f335248e6b5a9963e2420581e30
Jalilov <javohirjalilov777@gmail.com>
2021-11-29 22:35:40 +0500

    added test #03

--- test/test_build_func03.py
+++ test/test_build_func03.py
@@ -6,9 +6,9 @@ except ImportError:
 # checking answer value test#1
 def test_main_1():
     n = build_func03.n
-    assert n == 4, "Value Error 'number'"
+    assert n == 3.5, "Value Error 'number'"
 
 # checking answer value test#2
 def test_main_2():
     answer = build_func03.answer
-    assert answer == 4.0 , "Value Error  'answer'"
+    assert answer == 6.75 , "Value Error  'answer'"
